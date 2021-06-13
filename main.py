d8142442d6f46d7b3482792e3f38a124d2a78625
Adane <adaneadgo1@gmail.com>
2021-06-13 17:20:01 +0300

    integration

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import sympy as sp
 from sympy.utilities.lambdify import lambdify
 
 
+def Integration_Simpson_Method(f, n, rng):
+    f = lambdify(x, f)
+    a, b = rng[0], rng[1]
+    h = (b - a) / n
+    s = f(a) + f(b)
+    for i in range(1, n):
+        if i % 2:
+            s += 4*f(i*h)
+        else:
+            s += 2*f(i*h)
+    print("Integration Value = " + str(float((h/3) * s)))
+
+
 def Bisection_Method(f, little_range, epsilon):
     f = lambdify(x, f)
     a, b = little_range
@@ -96,15 +109,19 @@ def roots_Derivative_solver(pol, big_range, epsilon, step, method):
 
 x = sp.symbols('x')
 f = x*math.e**(-x**2 + 5*x)*(2*x**2 - 3*x - 5)
-rng = (0, 3)
+root_rng = (0, 3)
+integration_rng = [0.5, 1]
 epsilon = 0.0001
 step = 0.1
+n = 10
 
 print("f(x) = " + "x*math.e**(-x**2 + 5*x)*(2*x**2 - 3*x - 5)")
 print("Roots: [0,3]")
 print("Bisection Method:")
-roots_Solver(f, rng, epsilon, step, Bisection_Method)
+roots_Solver(f, root_rng, epsilon, step, Bisection_Method)
 print("Newton Raphson Method:")
-roots_Solver(f, rng, epsilon, step, Newton_Raphson)
+roots_Solver(f, root_rng, epsilon, step, Newton_Raphson)
 print("Integration: [0.5,1]")
+Integration_Simpson_Method(f, n, integration_rng)
+
 
